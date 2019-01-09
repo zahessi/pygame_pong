cfd3f5efa0a9b73393f8dbd319c17c8434a31d11
Alex <rievva.ara@gmail.com>
2019-01-09 23:17:29 +0200

    changed architecture

--- classes.py
+++ classes.py
@@ -1,5 +1,8 @@
 import pygame
-from random import random
+from random import random, sample
+from pygame import QUIT, USEREVENT, K_UP, K_DOWN
+
+FPS = 200
 
 WINDOWWIDTH = 1000
 WINDOWHEIGHT = 700
@@ -7,37 +10,114 @@ LINETHICKNESS = 10
 PADDLESIZE = 100
 PADDLEOFFSET = 20
 
-BLACK     = (0  ,0  ,0  )
-WHITE     = (255,255,255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 e1 = pygame.event.Event(pygame.USEREVENT, paddle=1)
 e2 = pygame.event.Event(pygame.USEREVENT, paddle=2)
 
-class Paddle(pygame.Rect):
 
-    def __init__(self, *props):
+def drawArena(DISPLAYSURF):
+    DISPLAYSURF.fill(BLACK)
+    pygame.draw.rect(DISPLAYSURF, WHITE,
+                     ((0, 0), (WINDOWWIDTH, WINDOWHEIGHT)), LINETHICKNESS*2)
+    pygame.draw.line(DISPLAYSURF, WHITE, (int(WINDOWWIDTH/2), 0),
+                     (int(WINDOWWIDTH/2), WINDOWHEIGHT), int(LINETHICKNESS/4))
+
+
+def get_random_color(): return tuple(sample(range(0, 256), 3))
+
+
+class Game:
+
+    def __init__(self):
+        pygame.init()
+
+        self.FPSCLOCK = pygame.time.Clock()
+        self.DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+        pygame.display.set_caption('Pong')
+
+    def setup_game(self):
+        playerOnePosition = playerTwoPosition = (WINDOWHEIGHT - PADDLESIZE) / 2
+
+        paddle1 = Paddle(self.DISPLAYSURF, PADDLEOFFSET, playerOnePosition,
+                        LINETHICKNESS, PADDLESIZE)
+        paddle2 = Paddle(self.DISPLAYSURF, WINDOWWIDTH - PADDLEOFFSET - LINETHICKNESS,
+                        playerTwoPosition, LINETHICKNESS, PADDLESIZE)
+        ball = Ball(self.DISPLAYSURF)
+        score = Score(self.DISPLAYSURF)
+
+        drawArena(self.DISPLAYSURF)
+        paddle1.draw()
+        paddle2.draw()
+        ball.draw()
+
+        return paddle1, paddle2, ball, score
+
+    def update_game_screen(self, paddle1, paddle2, ball, score):
+        keys = pygame.key.get_pressed()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == USEREVENT and event.paddle == 1:
+                paddle1.color = get_random_color()
+            elif event.type == USEREVENT and event.paddle == 2:
+                paddle2.color = get_random_color()
+
+        if keys[ord("w")]:
+            paddle1.y -= 2
+        elif keys[ord("s")]:
+            paddle1.y += 2
+
+        if keys[K_UP]:
+            paddle2.y -= 2
+        elif keys[K_DOWN]:
+            paddle2.y += 2
+
+        drawArena(self.DISPLAYSURF)
+        paddle1.draw()
+        paddle2.draw()
+        ball.draw()
+        ball.move(paddle1, paddle2)
+
+        score.update_score(paddle1, paddle2)
+
+        pygame.display.update()
+        self.FPSCLOCK.tick(FPS)
+
+class Paddle(pygame.Rect):
+    display = None
+
+    def __init__(self, display, *props):
         self.score = 0
+        self.color = WHITE
+        self.display = display
         pygame.Rect.__init__(self, props[0], props[1], props[2], props[3])
-    
-    def draw(self, color, DISPLAYSURF):
+
+    def draw(self):
         if self.bottom > WINDOWHEIGHT - LINETHICKNESS:
             self.bottom = WINDOWHEIGHT - LINETHICKNESS
         elif self.top < LINETHICKNESS:
             self.top = LINETHICKNESS
-        
-        pygame.draw.rect(DISPLAYSURF, color, self)
+
+        pygame.draw.rect(self.display, self.color, self)
 
 
 class Ball(pygame.Rect):
+    display = None
 
-    def __init__(self, *props):
+    def __init__(self, display, *props):
+        self.display = display
         self.pos_x = (WINDOWWIDTH - LINETHICKNESS)/2
         self.pos_y = (WINDOWHEIGHT - LINETHICKNESS)/2
         self.dir_x = self.dir_y = -1
-        pygame.Rect.__init__(self, self.pos_x, self.pos_y, LINETHICKNESS, LINETHICKNESS)
+        pygame.Rect.__init__(self, self.pos_x, self.pos_y,
+                             LINETHICKNESS, LINETHICKNESS)
 
-    def draw(self, display):
-        pygame.draw.rect(display, WHITE, self)
+    def draw(self):
+        pygame.draw.rect(self.display, WHITE, self)
 
     def move(self, *paddles):
         speed = random() + 1
@@ -55,7 +135,7 @@ class Ball(pygame.Rect):
 
     def check_hitball(self, paddle1, paddle2):
         vector = 1
-        
+
         if self.dir_x < 0:
             if abs(paddle1.right - self.left) <= 1.5 and paddle1.top <= self.top and paddle1.bottom >= self.bottom:
                 pygame.event.post(e1)
@@ -63,7 +143,7 @@ class Ball(pygame.Rect):
                 vector = -1
             elif self.x - LINETHICKNESS / 2 <= 0:
                 paddle1.score -= 1 if paddle1.score else 0
-        
+
         if self.dir_x > 0:
             if abs(paddle2.left - self.right) <= 1.5 and paddle2.top <= self.top and paddle2.bottom >= self.bottom:
                 pygame.event.post(e2)
@@ -76,15 +156,19 @@ class Ball(pygame.Rect):
 
 
 class Score:
+    display = None
 
-    def __init__(self):
+    def __init__(self, display):
+        self.display = display
         self.font = pygame.font.Font('freesansbold.ttf', 20)
 
-    def update_score(self, display, *paddles):
+    def update_score(self, *paddles):
         if len(paddles) == 2:
             for paddle in paddles:
-                result = self.font.render(f'Score = {paddle.score}', True, WHITE)
+                result = self.font.render(
+                    f'Score = {paddle.score}', True, WHITE)
                 rect = result.get_rect()
-                rect.topright = (150, 50) if not paddles.index(paddle) else (WINDOWWIDTH - 150 + rect.width, 50)
+                rect.topright = (150, 50) if not paddles.index(
+                    paddle) else (WINDOWWIDTH - 150 + rect.width, 50)
 
-                display.blit(result, rect) 
+                self.display.blit(result, rect)
--- code_which_works.py
+++ code_which_works.py
@@ -1,81 +1,12 @@
-import pygame, sys, random
-from pygame import QUIT, USEREVENT, K_UP, K_DOWN
-from classes import Paddle, Ball, Score
+from classes import Game
 
-FPS = 200
-
-WINDOWWIDTH = 1000
-WINDOWHEIGHT = 700
-LINETHICKNESS = 10
-PADDLESIZE = 100
-PADDLEOFFSET = 20
-
-BLACK     = (0  ,0  ,0  )
-WHITE     = (255,255,255)
-# background_image = pygame.image.load('bg.jpg')
-# background_image = pygame.transform.scale(background_image, (1000, 700))
-
-get_random_color = lambda: tuple(random.sample(range(0, 256), 3))
-
-def drawArena(DISPLAYSURF):
-    DISPLAYSURF.fill(BLACK)
-    pygame.draw.rect(DISPLAYSURF, WHITE, ((0,0),(WINDOWWIDTH,WINDOWHEIGHT)), LINETHICKNESS*2)
-    pygame.draw.line(DISPLAYSURF, WHITE, (int(WINDOWWIDTH/2),0),(int(WINDOWWIDTH/2),WINDOWHEIGHT), int(LINETHICKNESS/4))
 
 def main():
-    pygame.init()
+    game = Game() 
+    game_objects_pool = game.setup_game()
 
-    FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT)) 
-    pygame.display.set_caption('Pong')
+    while 1:
+        game.update_game_screen(*game_objects_pool)
 
-    playerOnePosition = playerTwoPosition = (WINDOWHEIGHT - PADDLESIZE) /2
-
-    paddle1 = Paddle(PADDLEOFFSET,playerOnePosition, LINETHICKNESS,PADDLESIZE)
-    paddle2 = Paddle(WINDOWWIDTH - PADDLEOFFSET - LINETHICKNESS, playerTwoPosition, LINETHICKNESS,PADDLESIZE)
-    ball = Ball()
-    score = Score()
-
-    drawArena(DISPLAYSURF)
-    paddle1.draw(WHITE, DISPLAYSURF)
-    paddle2.draw(WHITE, DISPLAYSURF)
-    ball.draw(DISPLAYSURF)
-
-    paddle1_color = paddle2_color = WHITE
-
-    while True:
-        keys = pygame.key.get_pressed()
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == USEREVENT and event.paddle==1:
-                paddle1_color = get_random_color()
-            elif event.type == USEREVENT and event.paddle==2:
-                paddle2_color = get_random_color()
-
-        if keys[ord("w")]:
-            paddle1.y -= 2
-        elif keys[ord("s")]:
-            paddle1.y += 2
-
-        if keys[K_UP]:
-            paddle2.y -= 2
-        elif keys[K_DOWN]:
-            paddle2.y += 2
-
-        drawArena(DISPLAYSURF)
-        paddle1.draw(paddle1_color, DISPLAYSURF)
-        paddle2.draw(paddle2_color, DISPLAYSURF)
-        ball.draw(DISPLAYSURF)   
-        ball.move(paddle1, paddle2)
-        
-        score.update_score(DISPLAYSURF, paddle1, paddle2)
-        # displayScore(DISPLAYSURF, BASICFONT, paddle1, paddle2)
-        
-        pygame.display.update()
-        FPSCLOCK.tick(FPS)
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
